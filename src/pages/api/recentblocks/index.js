52ea20bf4f75926cc7f28ce1ecf14b244a4cb962
Chan Jer Yong <chanjeryong@live.com>
2023-01-25 11:54:27 +0800

    updated

--- src/pages/api/recentblocks/index.js
+++
@@ -1,6 +0,0 @@
-import getRecentBlocks from "../../../utils/getRecentBlocks";
-
-export default async function handler(req, res) {
-    const blocks = await getRecentBlocks(15);
-    res.status(200).json(blocks);
-}
--- src/pages/api/recenttxns/index.js
+++
@@ -1,6 +0,0 @@
-import getRecentTxns from "../../../utils/getRecentTxns";
-
-export default async function handler(req, res) {
-    const txns = await getRecentTxns(2);
-    res.status(200).json(txns);
-}
